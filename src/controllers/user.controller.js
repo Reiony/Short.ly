facb24589d5091c71505ecaddc5eb5681018b6bd
reiony <reiony.dev@gmail.com>
2023-03-03 15:20:58 -0300

    feat: get users me

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcrypt";
 import { v4 as uuid } from "uuid";
 import { db } from "../database/database.js";
+import { shortenUrl } from "./url.controller.js";
 
 export async function signUpUser(req, res) {
   const { name, email, password } = req.body;
@@ -52,5 +53,42 @@ export async function signInUser(req, res) {
   } catch (err) {
     res.status(500).send(err.message);
     return;
+  }
+}
+
+export async function getUserProfilebyId(req, res) {
+  const session = res.locals.session; 
+  try {
+    const viewCounter = await db.query(
+      `
+    SELECT SUM("viewCount")
+    FROM urls 
+    WHERE urls."userId"=$1;`,
+      [session.rows[0].userId]
+    );
+
+    const urlsfromUserLoggedIn = await db.query(
+      `
+    SELECT id, "shortUrl", url, "viewCount"
+    FROM urls 
+    WHERE urls."userId"=$1;`,
+      [session.rows[0].userId]
+    );
+    const username = await db.query(
+      `
+    SELECT id, name 
+    FROM users 
+    WHERE users.id=$1;`,
+      [session.rows[0].userId]
+    );
+
+    res.send({
+      id: session.rows[0].userId,
+      name: username.rows[0],
+      viewCount: viewCounter.rows[0] || 0 ,
+      shortenedUrls: urlsfromUserLoggedIn.rows,
+    })
+  } catch (err) {
+    res.status(500).send(err.message);
   }
 }
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
-import { signInUser, signUpUser } from "../controllers/user.controller.js";
+import { getUserProfilebyId, signInUser, signUpUser } from "../controllers/user.controller.js";
+import { authValidation } from "../middlewares/authValidation.middleware.js";
 import { validateSchema } from "../middlewares/validateSchema.js";
 import { signInSchema, signUpSchema } from "../schemas/userSchema.js";
 
@@ -7,5 +8,6 @@ const userRoutes = Router();
 
 userRoutes.post("/signup", validateSchema(signUpSchema), signUpUser);
 userRoutes.post("/signin", validateSchema(signInSchema), signInUser);
+userRoutes.get("/users/me", authValidation, getUserProfilebyId)
 
 export default userRoutes;
