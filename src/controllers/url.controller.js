277f8326a75dbd68c60e7ce790856cda4f43710e
reiony <reiony.dev@gmail.com>
2023-03-03 14:15:36 -0300

    feat: get Url, shortened Url

--- src/controllers/url.controller.js
+++ src/controllers/url.controller.js
@@ -1,5 +1,5 @@
 import { nanoid } from "nanoid";
-import {db} from "../database/database.js"
+import { db } from "../database/database.js";
 export async function shortenUrl(req, res) {
   const { url } = req.body;
 
@@ -22,5 +22,46 @@ export async function shortenUrl(req, res) {
     res.status(201).send(shortUrlwithId.rows[0]);
   } catch (err) {
     res.status(500).send(err.message);
+  }
+}
+
+export async function getUrlDetails(req, res) {
+  try {
+    const { id } = req.params;
+    const Url = await db.query(
+      `
+        SELECT id, "shortUrl", url 
+        FROM urls WHERE id=$1`,
+      [id]
+    );
+    if (Url.rowCount === 0) {
+      res.sendStatus(404);
+    }
+    res.status(200).send(Url.rows[0]);
+  } catch (err) {
+    res.status(500).send(err.message);
+    return;
+  }
+}
+
+export async function getShortenedUrl(req, res) {
+  const { shortenedUrl } = req.params;
+
+  try {
+    const shUrl = await db.query(
+      `
+    SELECT * 
+    FROM urls 
+    WHERE "shortUrl"=$1`,
+      [shortenedUrl]
+    );
+    if (shUrl.rowCount===0){
+        return res.sendStatus(404);
+    }
+    await db.query(`UPDATE urls SET viewCount = viewCount + 1 WHERE id=$1`,[shUrl.rows[0].id])
+    res.redirect(shUrl.rows[0].url);
+  } catch (err) {
+    res.status(500).send(err.message);
+    return;
   }
 }
--- src/routes/urls.routes.js
+++ src/routes/urls.routes.js
@@ -1,14 +1,16 @@
 import { Router } from "express";
-import { shortenUrl } from "../controllers/url.controller.js";
+import { getShortenedUrl, getUrlDetails, shortenUrl } from "../controllers/url.controller.js";
 import { authValidation } from "../middlewares/authValidation.middleware.js";
+import { validateSchema } from "../middlewares/validateSchema.js";
+import { urlSchema } from "../schemas/urlSchema.js"
 
 
 const urlRouter = Router();
 
-urlRouter.post("/urls/shorten", authValidation, shortenUrl)
-/* urlRouter.post("/urls/:id",)
-urlRouter.get("/urls/open/:shortUrl",)
-urlRouter.delete("/urls/:id",) 
-urlRouter.get("/ranking",) */
+urlRouter.post("/urls/shorten", authValidation, validateSchema(urlSchema), shortenUrl)
+urlRouter.get("/urls/:id", getUrlDetails)
+urlRouter.get("/urls/open/:shortUrl", getShortenedUrl)
+/* urlRouter.delete("/urls/:id",) 
+urlRouter.get("/ranking",)  */
 
 export default urlRouter;
